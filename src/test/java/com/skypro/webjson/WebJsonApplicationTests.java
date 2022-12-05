a2c2bd8f82dc99aad8f4ade15dc20f4323a17249
ArtemA1ekseev <a1ekseevartem.ru@gmail.com>
2022-12-05 20:29:12 +0400

    initial commit.

---
+++ src/test/java/com/skypro/webjson/WebJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.webjson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebJsonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
